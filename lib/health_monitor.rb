9f8fa4dc389630a2d2d07c61374ceac4a7d81f9f
Marek Waligórski <marek.waligorski@netguru.pl>
2016-10-26 22:12:29 +0200

    simplify database file

--- lib/health_monitor.rb
+++ lib/health_monitor.rb
@@ -3,38 +3,11 @@ require 'hanami/mailer'
 Dir["#{ __dir__ }/health_monitor/**/*.rb"].each { |file| require_relative file }
 
 Hanami::Model.configure do
-  ##
-  # Database adapter
-  #
-  # Available options:
-  #
-  #  * Memory adapter
-  #    adapter type: :memory, uri: 'memory://localhost/health_monitor_development'
-  #
-  #  * SQL adapter
-  #    adapter type: :sql, uri: 'sqlite://db/health_monitor_development.sqlite3'
-  #    adapter type: :sql, uri: 'postgres://localhost/health_monitor_development'
-  #    adapter type: :sql, uri: 'mysql://localhost/health_monitor_development'
-  #
-  adapter type: :sql, uri: ENV['HEALTH_MONITOR_DATABASE_URL']
+  adapter type: :sql, uri: ENV['DATABASE_URL']
 
-  ##
-  # Migrations
-  #
   migrations 'db/migrations'
   schema     'db/schema.sql'
 
-  ##
-  # Database mapping
-  #
-  # Intended for specifying application wide mappings.
-  #
-  # You can specify mapping file to load with:
-  #
-  # mapping "#{__dir__}/config/mapping"
-  #
-  # Alternatively, you can use a block syntax like the following:
-  #
   mapping do
   end
 end.load!
