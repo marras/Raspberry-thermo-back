612295f8af857808488653fc7b363dfa8b8a5b51
Marek Waligórski <marek.waligorski@netguru.pl>
2016-10-27 16:05:04 +0200

    Display current (last) temperatures

--- apps/web/views/data/index.rb
+++ apps/web/views/data/index.rb
@@ -1,5 +1,16 @@
 module Web::Views::Data
   class Index
     include Web::View
+
+    def current_temps
+      Metric.all.map do |metric|
+        temp = Point
+          .where(metric_id: metric.id)
+          .order(:day)
+          .last
+          .value
+        [ metric.name, temp ]
+      end
+    end
   end
 end
