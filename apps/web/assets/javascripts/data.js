6f76259efc5eb8b303f1c8295de1865ecc2d3430
Marek Waligórski <marek.waligorski@netguru.pl>
2016-10-27 13:11:08 +0200

    Remove chartist, embed example chart from Dygraph

--- apps/web/assets/javascripts/data.js
+++
@@ -1,4 +0,0 @@
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-new Chartist.Line('.ct-chart', window.data);
