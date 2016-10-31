b7ab681918e743f27994d44ebb8f4e7e2ac9414a
Marek Waligórski <marek.waligorski@netguru.pl>
2016-10-31 12:49:58 +0100

    Setup for capistrano on 150.254.80.131 (marta)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,49 +1,33 @@
-# config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'thermo_monitor'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :scm, :git
+set :repo_url, 'git@github.com:marras/Raspberry-thermo-back.git'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/deploy/thermo_monitor'
 
-# Default value for :scm is :git
-# set :scm, :git
+set :keep_releases, 5
+set :log_level, :debug
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :linked_files, %w{.env}
+set :linked_dirs, %w{log}
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc "Precompile Hanami assets"
+  task :precompile do
+    on roles(:all) do
+      execute "cd #{fetch(:deploy_to)}/current && HANAMI_ENV=production ~/.rvm/bin/rvm default do bundle exec hanami assets precompile"
     end
   end
 
+  desc "Restart Puma server"
+  task :restart_server do
+    on roles(:all) do
+      execute "sudo service puma-manager restart"
+    end
+  end
+
+  after :published, :precompile
+  after :precompile, :restart_server
 end
