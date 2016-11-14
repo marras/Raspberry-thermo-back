0eff20cd278074dd8b05f626e35951b4e321a52e
Marek Waligórski <marek.waligorski@netguru.pl>
2016-11-14 13:11:44 +0100

    Display calibrated 'last values' in top part of the interface

--- apps/web/models/point.rb
+++ apps/web/models/point.rb
@@ -1,3 +1,15 @@
 class Point < Sequel::Model
   many_to_one :metric
+
+  def self.last_calibrated(metric)
+    calibration = metric.calibration
+
+    value = self
+      .where(metric_id: metric.id)
+      .order(:day)
+      .last
+      .value
+
+    value + calibration
+  end
 end
--- apps/web/views/data/index.rb
+++ apps/web/views/data/index.rb
@@ -4,13 +4,13 @@ module Web::Views::Data
   class Index
     include Web::View
 
+    def calibrations
+      Metric.all.map(&:calibration)
+    end
+
     def current_temps
       Metric.all.map do |metric|
-        temp = Point
-          .where(metric_id: metric.id)
-          .order(:day)
-          .last
-          .value
+        temp = Point.last_calibrated(metric)
         [ metric.name, temp ]
       end
     end
